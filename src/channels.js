781475ed6476e08c99d76baa8fdc86e88268ae4f
Emmie Chew <emmie.chew@student.unsw.edu.au>
2022-06-10 00:40:06 +1000

    Rearranged the order of functions

--- src/channels.js
+++ src/channels.js
@@ -2,11 +2,11 @@ function channelsCreateV1(authUserId, name, isPublic) {
     return 'authUserId' + 'name' + 'isPublic';
 }
 
-function channelsListallV1(authUserId) {
+function channelsListV1(authUserId) {
     return 'authUserId';
 }
 
-function channelsListV1(authUserId) {
+function channelsListallV1(authUserId) {
     return 'authUserId';
 }
 
