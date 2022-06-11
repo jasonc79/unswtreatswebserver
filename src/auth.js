d47098050c58357712d2f5014edf5445056ecb5e
Giuliana De Bellis <z5309331@unsw.edu.au>
2022-06-11 16:18:51 +1000

    Results push

--- src/auth.js
+++
@@ -1,7 +0,0 @@
-function authLoginV1(email, password) {
-    return 'email' + 'password';
-}
-
-function authRegisterV1(email, password, nameFirst, nameLast) {
-    return 'email' + 'password' + 'nameFirst' + 'nameLast'; 
-}
