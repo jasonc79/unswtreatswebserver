b31eed38b3224f53bed599c1b5b91dca8788de60
Your Name <email@example.com>
2022-06-16 21:38:06 +1000

    created new stub for channel.js

--- src/channel.js
+++ src/channel.js
@@ -1,14 +1,26 @@
-
-
-function channelJoinV1(authUserId, channelId) {
-    return 'authUserId' + 'channelId';
-}
-function channelInviteV1(authUserId, channelId, uId) {
-    return 'authUserId' + 'channelId' + 'uId';
-}
 function channelDetailsV1(authUserId, channelId) {
-    return 'authUserId' + 'channelId';
-}
-function channelMessagesV1(authUserId, channelId, start) {
-    return 'authUserId' + 'channelId' + 'start';
-}
+    return {
+      name: 'secret candy crush team', 
+      isPublic: true,
+      ownerMembers: [],
+      allMembers: [],
+    };
+  }
+  
+  function channelJoinV1(authUserId, channelId) {
+    return {};
+  }
+  
+  function channelInviteV1(authUserId, channelId, uId) {
+    return {};
+  }
+  
+  function channelMessagesV1(authUserId, channelId, start) {
+    return {
+      messages: [],
+      start: 0,
+      end: -1,
+    };
+  }
+  
+  export { channelDetailsV1, channelJoinV1, channelInviteV1, channelMessagesV1 };
