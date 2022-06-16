11c4421bad40d47b289db72e44711f9ba1988e0e
Jason Cheung <jason.cheung4@student.unsw.edu.au>
2022-06-16 21:50:40 +1000

    Revert "Merge branch 'CM/Replace_Stub_channels' into 'master'"
    This reverts merge request !22

--- src/channels.js
+++ src/channels.js
@@ -1,19 +1,12 @@
 function channelsCreateV1(authUserId, name, isPublic) {
-    return {
-      channelId: 1,
-    };
-  }
-  
-  function channelsListV1(authUserId) {
-    return {
-      channels: [] // see interface for contents
-    };
-  }
-  
-  function channelsListallV1(authUserId) {
-    return {
-      channels: [] // see interface for contents
-    };
-  }
-  
-  export { channelsCreateV1, channelsListV1, channelsListallV1 };
+    return 'authUserId' + 'name' + 'isPublic';
+}
+
+function channelsListV1(authUserId) {
+    return 'authUserId';
+}
+
+function channelsListallV1(authUserId) {
+    return 'authUserId';
+}
+
