e027938f8dfa77721c2426bd1bb4d85c4d1df928
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-21 11:00:05 +1000

    Completed tests for channelsCreateV1 and channelsListV1

---
+++ src/channels.test.js
@@ -0,0 +1,47 @@
+import { authRegisterV1, channelsCreateV1 } from "./channels.js";
+import { channelsListV1 } from './channels.js';
+
+beforeEach(() => {
+    clearV1();
+});
+
+describe("Testing channelsCreateV1", () => {
+    test('Correct Name, is public', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsCreateV1(authUserId.authUserId, 'correct name', true)).toStrictEqual(
+            expect.objectContaining({
+                channelId: expect.any(Number)
+            })
+        );
+    });
+    test('Correct Name, is private', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsCreateV1(authUserId.authUserId, 'correct name', false)).toStrictEqual(
+            expect.objectContaining({
+                channelId: expect.any(Number)
+            })
+        );
+    });
+    test('Incorrect Name (too small)', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsCreateV1(authUserId.authUserId, '', true)).toStrictEqual({ error: 'error' });
+    });
+    test('Incorrect Name (too large)', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsCreateV1(authUserId.authUserId, 'very long channel name', true)).toStrictEqual({ error: 'error' });
+    });
+});
+
+describe("Testing channelsListV1", () => {
+    test('Correct return', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsCreateV1(authUserId.authUserId)).toStrictEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    channelId: expect.any(Number),
+                    name: expect.any(String),
+                })
+            ])
+        );
+    });
+});
