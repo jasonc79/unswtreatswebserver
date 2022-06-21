f58e8740556bc5fbdcf805b2bed1f24c10c5e355
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-21 12:07:19 +1000

    completed implementation for channelsCreateV1

--- src/channels.js
+++ src/channels.js
@@ -1,6 +1,10 @@
 function channelsCreateV1(authUserId, name, isPublic) {
+    if (name.length < 1 || name.length > 20) {
+        return { error: 'error' };
+    }
+    const channelId = Math.floor(1000 + Math.random() * 9000);
     return {
-      channelId: 1,
+        channelId: channelId,
     };
   }
   
@@ -17,3 +21,5 @@ function channelsCreateV1(authUserId, name, isPublic) {
   }
   
   export { channelsCreateV1, channelsListV1, channelsListallV1 };
+
+console.log(channelsCreateV1(10, 'name', true));
