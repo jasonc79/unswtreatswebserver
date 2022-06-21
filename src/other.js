a43c40188c86353bf51178ef4390bf7fe131985b
Emmie <z5360408@ad.unsw.edu.au>
2022-06-21 22:40:38 +1000

    Implemented clearV1

--- src/other.js
+++ src/other.js
@@ -1,4 +1,10 @@
+import {getData, setData} from './dataStore.js';
 function clearV1() {
+  let data = getData();
+  data.users = [];
+  data.channels = [];
+  data.messages = [];
+  setData(data);
   return {};
 }
 
