fd2837fdfd23c0a2559b966919e1d0613be5e5a8
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-23 01:01:11 +1000

    completed tests and implementation for channelsCreateV1, channelsListV1 and userProfileV1

--- src/channels.js
+++ src/channels.js
@@ -1,19 +1,66 @@
+import { getData, setData } from './dataStore.js'
+
 function channelsCreateV1(authUserId, name, isPublic) {
+    if (name.length < 1 || name.length > 20) {
+        return { error: 'error' };
+    }
+
+    let data = getData();
+    let channels = data.channels;
+    let user;
+    const channelId = channels.length;
+
+    for (let person of data.users) {
+        if (person.uId === authUserId) {
+            user = person;
+        }
+    }
+
+    let newChannel = {
+        channelId: channelId,
+        name: name,
+        messages: [],
+        allMembers: [person],
+        staffMembers: [person],
+        isPublic: isPublic,
+    }
+
+    channels.push(newChannel);
+    setData(channels);
+
     return {
-      channelId: 1,
+        channelId: channelId,
     };
-  }
-  
-  function channelsListV1(authUserId) {
+}
+
+function channelsListV1(authUserId) {
+    let data = getData();
+    const user;
+    for (let person of data.users) {
+        if (person.uId === authUserId) {
+            user = person;
+        }
+    }
+
+    let channels = [];
+    for (let channel of data.channels) {
+        for (let member of channel.allMembers) {
+            if (member === user) {
+                channels.push(channel);
+                break;
+            }
+        }
+    }
+
     return {
-      channels: [] // see interface for contents
+        channels
     };
-  }
+}
   
-  function channelsListallV1(authUserId) {
+function channelsListallV1(authUserId) {
     return {
-      channels: [] // see interface for contents
+        channels: [] // see interface for contents
     };
-  }
+}
   
-  export { channelsCreateV1, channelsListV1, channelsListallV1 };
+export { channelsCreateV1, channelsListV1, channelsListallV1 };
--- src/users.js
+++ src/users.js
@@ -1,11 +1,20 @@
+import { getData } from './dataStore.js'
+
 function userProfileV1(authUserId, uId) {
-  return {
-    uId: 1, 
-    email: 'example@gmail.com',
-    nameFirst: 'Hayden', 
-    nameLast: 'Smith', 
-    handleStr: 'haydensmith'
-  }
+    let data = getData();
+
+    for (let id in data.users.uId) {
+        if (id === uId) {
+            return {
+                uId: uId, 
+                email: uId.email,
+                nameFirst: uId.nameFirst, 
+                nameLast: uId.nameLast, 
+                handleStr: uId.handleStr,
+            }
+        }
+    }
+    return { error: 'error' };
 }
 
 export { userProfileV1 }
