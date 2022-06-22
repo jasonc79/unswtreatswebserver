3e62aa68fd9f243ad780e542a8741269fe455a64
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-22 11:18:31 +1000

    fixing files and tests for merging

--- src/channels.js
+++ src/channels.js
@@ -1,41 +1,19 @@
-import { getData, setData } from './dataStore.js'
-
 function channelsCreateV1(authUserId, name, isPublic) {
-    if (name.length < 1 || name.length > 20) {
-        return { error: 'error' };
-    }
-    const channelId = Math.floor(1000 + Math.random() * 9000);
-    let newChannel = {
-        channelId: channelId,
-        name: name,
-        allMembers: [authUserId.authUserId],
-        staffMembers: [authUserId.authUserId],
-        isPublic: isPublic,
-    }
-
-    let data = getData();
-    let channels = data.channels;
-
-    channels.push(newChannel)
-    //console.log(channels);
-
-    setData(channels);
-
     return {
-        channelId: channelId,
+      channelId: 1,
     };
-}
-
-function channelsListV1(authUserId) {
-    return {
-        channels: [] // see interface for contents
-    };
-}
+  }
   
-function channelsListallV1(authUserId) {
+  function channelsListV1(authUserId) {
     return {
-        channels: [] // see interface for contents
+      channels: [] // see interface for contents
     };
-}
+  }
   
-export { channelsCreateV1, channelsListV1, channelsListallV1 };
+  function channelsListallV1(authUserId) {
+    return {
+      channels: [] // see interface for contents
+    };
+  }
+  
+  export { channelsCreateV1, channelsListV1, channelsListallV1 };
