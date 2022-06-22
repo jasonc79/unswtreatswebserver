4e22ec0452f23b38af565e31829decab6e95df77
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-22 11:09:44 +1000

    added created channel to the data store

--- src/channels.js
+++ src/channels.js
@@ -1,25 +1,41 @@
+import { getData, setData } from './dataStore.js'
+
 function channelsCreateV1(authUserId, name, isPublic) {
     if (name.length < 1 || name.length > 20) {
         return { error: 'error' };
     }
     const channelId = Math.floor(1000 + Math.random() * 9000);
+    let newChannel = {
+        channelId: channelId,
+        name: name,
+        allMembers: [authUserId.authUserId],
+        staffMembers: [authUserId.authUserId],
+        isPublic: isPublic,
+    }
+
+    let data = getData();
+    let channels = data.channels;
+
+    channels.push(newChannel)
+    //console.log(channels);
+
+    setData(channels);
+
     return {
         channelId: channelId,
     };
-  }
-  
-  function channelsListV1(authUserId) {
-    return {
-      channels: [] // see interface for contents
-    };
-  }
-  
-  function channelsListallV1(authUserId) {
-    return {
-      channels: [] // see interface for contents
-    };
-  }
-  
-  export { channelsCreateV1, channelsListV1, channelsListallV1 };
+}
 
-console.log(channelsCreateV1(10, 'name', true));
+function channelsListV1(authUserId) {
+    return {
+        channels: [] // see interface for contents
+    };
+}
+  
+function channelsListallV1(authUserId) {
+    return {
+        channels: [] // see interface for contents
+    };
+}
+  
+export { channelsCreateV1, channelsListV1, channelsListallV1 };
