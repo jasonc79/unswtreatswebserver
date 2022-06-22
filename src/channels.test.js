9994f0c8e7f614a6ba74bef65d060f9b5f583a28
Charlotte Masuda <z5359445@ad.unsw.edu.au>
2022-06-23 02:13:01 +1000

    fixed expects in channels.test.js and made channels.js more efficient

--- src/channels.test.js
+++ src/channels.test.js
@@ -1,6 +1,8 @@
 import { authRegisterV1 } from "./auth.js"
 import { channelsCreateV1, channelsListV1 } from "./channels.js";
 import { clearV1 } from "./other.js"
+import { userProfileV1 } from "./users.js"
+import { getData } from "./dataStore.js"
 
 beforeEach(() => {
     clearV1();
@@ -34,15 +36,29 @@ describe("Testing channelsCreateV1", () => {
 });
 
 describe("Testing channelsListV1", () => {
-    test('Correct return', () => {
+    test('Correct return all channels', () => {
         const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
-        expect(channelsCreateV1(authUserId.authUserId)).toStrictEqual(
-            expect.arrayContaining([
-                expect.objectContaining({
-                    channelId: expect.any(Number),
-                    name: expect.any(String),
-                })
-            ])
-        );
+        const channel1 = channelsCreateV1(authUserId.authUserId, 'name1', true);
+        const channel2 = channelsCreateV1(authUserId.authUserId, 'name2', true);
+        const data = getData();
+        expect(channelsListV1(authUserId.authUserId)).toStrictEqual(data.channels);
+    });
+    test('Correct return no channels', () => {
+        const authUserId = authRegisterV1('email@gmail.com', 'password', 'firstname', 'lastname');
+        expect(channelsListV1(authUserId.authUserId)).toStrictEqual([]);
+    });
+    test('Correct return some channels', () => {
+        const authUserId1 = authRegisterV1('email1@gmail.com', 'password1', 'firstname1', 'lastname1');
+        const authUserId2 = authRegisterV1('email2@gmail.com', 'password2', 'firstname2', 'lastname2');
+        const channel1 = channelsCreateV1(authUserId1.authUserId, 'name1', true);
+        const channel2 = channelsCreateV1(authUserId2.authUserId, 'name2', true);
+        expect(channelsListV1(authUserId1.authUserId)).toStrictEqual({
+            channelId: channel1.channelId,
+            name: 'name1',
+            messages: [],
+            allMembers: [userProfileV1(authUserId2.authUserId, authUserId1.authUserId)],
+            staffMembers:[userProfileV1(authUserId2.authUserId, authUserId1.authUserId)],
+            isPublic: true,
+        });
     });
 });
