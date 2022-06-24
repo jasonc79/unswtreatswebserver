30760d06bb2a7af9003ea799c29210e0761b789b
Maynarde <maynarde.joseph@gmail.com>
2022-06-24 14:56:14 +1000

    added proper documentation

--- src/channel.js
+++ src/channel.js
@@ -17,10 +17,32 @@ function channelJoinV1(authUserId, channelId) {
 function channelInviteV1(authUserId, channelId, uId) {
   return {};
 }
+
 /*
-This function starting from start returns 50 messages from a specified channel
+This function checks if authUserId, channelId are valid then starting from start 
+returns 50 messages from a specified channel. If there are less than start+50
+messages it returns -1 in the "end: " 
+
+Arguments:
+    authUserId (number)   - The email inputted by the user
+    channelId (number)    - The password inputted by the user
+    start(number)         - The index of messages you wish to start from
+
+Return Value:
+    Returns {error: 'error'} on invalid authUserId
+    Returns {error: 'error'} on invalid channelId
+    Returns {error: 'error'} on valid channelId but authUserId is not part of members
+    Returns {error: 'error'} on start is greater than ammount of messages in messages
+    Returns {messages: messages, start: start, end: -1} when all inputs are valid but 
+    there are less than start+50 messages in messages
+    Returns {messages: messages, start: start, end: start+50} when all inputs are 
+    valid and there are more or start+50 messages in messages 
 */
+
 function channelMessagesV1(authUserId, channelId, start) {
+  if (!checkValidId(authUserId)) {
+    return { error: "error" };
+  }
   const data = getData();
   if (!checkValidChannel(channelId)) {
     return { error: "error" };
