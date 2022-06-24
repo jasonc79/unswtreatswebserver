5b18b210af869ff0f6f40d486d279fd798ddf513
Alexa Platon <z5367318@ad.unsw.edu.au>
2022-06-24 12:26:09 +1000

    Fixed up channel.test.js according to comments made

--- src/channel.test.js
+++ src/channel.test.js
@@ -14,32 +14,32 @@ describe('Testing channelInviteV1', () =>  {
     const channelID = channelsCreateV1(authUserID1, 'Channel1', true);
     
     test ('Valid inputs', () => {
-        channelJoinV1(authUserID1, channelID); 
-        const validInput = channelInviteV1(authUserID1, channelID, authUserID2); 
+        channelJoinV1(authUserID1.authUserId, channelID.channelId); 
+        const validInput = channelInviteV1(authUserID1.authUserId, channelID.channelId, authUserID2.authUserId); 
         expect(validInput).toEqual({}); 
     }); 
     
     test ('Invalid channelID', () => {
-        channelJoinV1(authUserID1, channelID);
-        const invalidChannelID = channelInviteV1(authUserID1, -1, authUserID2); 
+        channelJoinV1(authUserID1.authUserId, channelID.channelId);
+        const invalidChannelID = channelInviteV1(authUserID1.authUserId, -1, authUserID2.authUserId); 
         expect(invalidChannelID).toEqual({ error: 'error' }); 
     }); 
     
     test ('Invalid userID', () => {
-        channelJoinV1(authUserID1, channelID);
-        const invalidUserID = channelInviteV1(authUserID1, channelID, -1); 
+        channelJoinV1(authUserID1.authUserId, channelID.channelId);
+        const invalidUserID = channelInviteV1(authUserID1.authUserId, channelID.channelId, -1); 
         expect(invalidUserID).toEqual({ error: 'error' }); 
     }); 
     
     test ('User is already a member', () => {
-        channelJoinV1(authUserID1, channelID);
-        channelJoinV1(authUserID2, channelID); 
-        const alreadyMember = channelInviteV1(authUserID1, channelID, authUserID2); 
+        channelJoinV1(authUserID1.authUserId, channelID.channelId);
+        channelJoinV1(authUserID2.authUserId, channelID.channelId); 
+        const alreadyMember = channelInviteV1(authUserID1.authUserId, channelID.channelId, authUserID2.authUserId); 
         expect(alreadyMember).toEqual({ error: 'error' }); 
     }); 
     
     test ('Authorised user is not a member', () => {
-        const notMember = channelInviteV1(authUserID1, channelID, authUserID2); 
+        const notMember = channelInviteV1(authUserID1.authUserId, channelID.channelId, authUserID2.authUserId); 
         expect(notMember).toEqual({ error: 'error' }); 
     })
 })
