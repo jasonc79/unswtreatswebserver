35c4cb1af1050d5b32f61439b18108f85ca46e91
Emmie <z5360408@ad.unsw.edu.au>
2022-07-14 23:40:49 +1000

    Moved some test functions to a helper function to reduce clutter in test files

---
+++ src/helperTests.ts
@@ -0,0 +1,82 @@
+import request, { HttpVerb } from 'sync-request';
+import config from './config.json';
+
+const OK = 200;
+const port = config.port;
+const url = config.url;
+
+// Return types
+export type authUser = {
+  token: string,
+  authUserId: number
+}
+export const errorMsg = { error: 'error' };
+
+
+function requestHelper(method: HttpVerb, path: string, payload: object) {
+  let qs = {};
+  let json = {};
+  if (['GET', 'DELETE'].includes(method)) {
+    qs = payload;
+  } else {
+    json = payload;
+  }
+  return request(method, url + ':' + port + path, { qs, json });
+}
+
+// ========================================================================= //
+// WRAPPER FUNCTIONS
+// ==========================================================================//
+
+// Auth Functions
+export function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
+  const res = requestHelper('POST', '/auth/register/v2', {
+    email: email,
+    password: password,
+    nameFirst: nameFirst,
+    nameLast: nameLast
+  });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+
+export function requestAuthLogin(email: string, password: string) {
+  const res = requestHelper('POST', '/auth/login/v2', {
+    email: email,
+    password: password,
+  });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+// Channels functions
+export function requestChannelCreate(token: string, name: string, isPublic: boolean) {
+  const res = requestHelper('POST', '/channels/create/v2', { token, name, isPublic });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+
+export function requestChannelsList(token: string) {
+  const res = requestHelper('GET', '/channels/list/v2', { token });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+
+export function requestChannelsListAll(token: string) {
+  const res = requestHelper('GET', '/channels/listall/v2', { token });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+
+// User
+export function requestUserProfile(token: string, uId: number) {
+  const res = requestHelper('GET', '/user/profile/v2', { token, uId });
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
+
+// Other
+export function requestClear() {
+  const res = requestHelper('DELETE', '/clear/v1', {});
+  expect(res.statusCode).toBe(OK);
+  return JSON.parse(String(res.getBody()));
+}
--- src/users.test.ts
+++ src/users.test.ts
@@ -1,57 +1,17 @@
-import request, { HttpVerb } from 'sync-request';
-import config from './config.json';
+import { authUser, requestAuthRegister, errorMsg, requestClear } from './helperTests';
+import { requestUserProfile } from './helperTests';
 
-const OK = 200;
-const port = config.port;
-const url = config.url;
-const errorMsg = { error: 'error' };
-
-function requestHelper(method: HttpVerb, path: string, payload: object) {
-  let qs = {};
-  let json = {};
-  if (['GET', 'DELETE'].includes(method)) {
-    qs = payload;
-  } else {
-    json = payload;
-  }
-  return request(method, url + ':' + port + path, { qs, json });
-}
-
-// ========================================================================= //
-// Wrapper Functions
-
-function requestUserProfile(token: string, uId: number) {
-  return requestHelper('GET', '/user/profile/v2', { token, uId });
-}
-
-function requestAuthRegister(email: string, password: string, nameFirst: string, nameLast: string) {
-  return requestHelper('POST', '/auth/register/v2', {
-    email: email,
-    password: password,
-    nameFirst: nameFirst,
-    nameLast: nameLast
-  });
-}
-
-function requestClear() {
-  return requestHelper('DELETE', '/clear/v1', {});
-}
+let authUser: authUser;
 
 beforeEach(() => {
   requestClear();
+  authUser = requestAuthRegister('emai1@gmail.com', 'password1', 'firstname1', 'lastname1');
 });
 
 describe('Testing userProfileV1', () => {
   test('Valid uId', () => {
-    const res1 = requestAuthRegister('emai1@gmail.com', 'password1', 'firstname1', 'lastname1');
-    const authUser = JSON.parse(String(res1.getBody(('utf-8'))));
-    expect(res1.statusCode).toBe(OK);
-    const res2 = requestAuthRegister('email2@gmail.com', 'password2', 'firstname2', 'lastname2');
-    const uId = JSON.parse(String(res2.getBody(('utf-8'))));
-    expect(res2.statusCode).toBe(OK);
-    const res3 = requestUserProfile(authUser.token, uId.authUserId);
-    const profile = JSON.parse(String(res3.getBody(('utf-8'))));
-    expect(res3.statusCode).toBe(OK);
+    const uId = requestAuthRegister('email2@gmail.com', 'password2', 'firstname2', 'lastname2');
+    const profile = requestUserProfile(authUser.token, uId.authUserId);
     expect(profile).toStrictEqual({
       user: {
         uId: uId.authUserId,
@@ -63,13 +23,8 @@ describe('Testing userProfileV1', () => {
     });
   });
   test('Invalid uId', () => {
-    const res1 = requestAuthRegister('emai1@gmail.com', 'password1', 'firstname1', 'lastname1');
-    const authUser = JSON.parse(String(res1.getBody(('utf-8'))));
-    expect(res1.statusCode).toBe(OK);
     const uId = authUser.authUserId + 1;
-    const res2 = requestUserProfile(authUser.token, uId);
-    const profile = JSON.parse(String(res2.getBody(('utf-8'))));
-    expect(res2.statusCode).toBe(OK);
+    const profile = requestUserProfile(authUser.token, uId);
     expect(profile).toStrictEqual(errorMsg);
   });
 });
